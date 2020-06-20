fc1c8d105d7459ede7866fa1abc50633ca70a425
Deep Panara <deep@aspl.in>
2020-06-20 09:48:45 +0530

    9

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -11,6 +11,7 @@
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -58,9 +59,31 @@ class Category
      **/
     protected $parent;
 
+    private $lft;
+
+    private $rgt;
+
+    private $lvl;
+
+    private $slug;
+
+    private $title;
+
+    private $description;
+
+    private $h1Title;
+
+    private $metaDescription;
+
+    private $synonyms;
+
+    private $treeRoot;
+
     public function __construct()
     {
         $this->products = new ArrayCollection();
+        $this->listingType = new ArrayCollection();
+        $this->page = new ArrayCollection();
     }
 
     public function __toString()
@@ -167,4 +190,178 @@ class Category
         $this->products->removeElement($product);
         $product->removeCategory($this);
     }
+
+    public function getLft(): ?int
+    {
+        return $this->lft;
+    }
+
+    public function setLft(int $lft): self
+    {
+        $this->lft = $lft;
+
+        return $this;
+    }
+
+    public function getRgt(): ?int
+    {
+        return $this->rgt;
+    }
+
+    public function setRgt(int $rgt): self
+    {
+        $this->rgt = $rgt;
+
+        return $this;
+    }
+
+    public function getLvl(): ?int
+    {
+        return $this->lvl;
+    }
+
+    public function setLvl(int $lvl): self
+    {
+        $this->lvl = $lvl;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getH1Title(): ?string
+    {
+        return $this->h1Title;
+    }
+
+    public function setH1Title(?string $h1Title): self
+    {
+        $this->h1Title = $h1Title;
+
+        return $this;
+    }
+
+    public function getMetaDescription(): ?string
+    {
+        return $this->metaDescription;
+    }
+
+    public function setMetaDescription(?string $metaDescription): self
+    {
+        $this->metaDescription = $metaDescription;
+
+        return $this;
+    }
+
+    public function getSynonyms(): ?string
+    {
+        return $this->synonyms;
+    }
+
+    public function setSynonyms(?string $synonyms): self
+    {
+        $this->synonyms = $synonyms;
+
+        return $this;
+    }
+
+    public function getTreeRoot(): ?self
+    {
+        return $this->treeRoot;
+    }
+
+    public function setTreeRoot(?self $treeRoot): self
+    {
+        $this->treeRoot = $treeRoot;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ListingType[]
+     */
+    public function getListingType(): Collection
+    {
+        return $this->listingType;
+    }
+
+//    public function addListingType(ListingType $listingType): self
+//    {
+//        if (!$this->listingType->contains($listingType)) {
+//            $this->listingType[] = $listingType;
+//        }
+//
+//        return $this;
+//    }
+//
+//    public function removeListingType(ListingType $listingType): self
+//    {
+//        if ($this->listingType->contains($listingType)) {
+//            $this->listingType->removeElement($listingType);
+//        }
+//
+//        return $this;
+//    }
+
+//    /**
+//     * @return Collection|Page[]
+//     */
+//    public function getPage(): Collection
+//    {
+//        return $this->page;
+//    }
+//
+//    public function addPage(Page $page): self
+//    {
+//        if (!$this->page->contains($page)) {
+//            $this->page[] = $page;
+//            $page->addCategory($this);
+//        }
+//
+//        return $this;
+//    }
+//
+//    public function removePage(Page $page): self
+//    {
+//        if ($this->page->contains($page)) {
+//            $this->page->removeElement($page);
+//            $page->removeCategory($this);
+//        }
+//
+//        return $this;
+//    }
 }
